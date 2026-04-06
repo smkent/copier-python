a73157dff9482ad3f65088dba98ecab189568cfe
Stephen Kent <smkent@smkent.net>
2026-04-05 17:53:46 -0700

    Rename misnamed `pyproject.toml` snapshot test

--- tests/test_pyproject.py
+++ tests/test_pyproject.py
@@ -13,7 +13,7 @@ from syrupy.assertion import SnapshotAssertion
     "enable_docs",
     [pytest.param(True, id="docs"), pytest.param(False, id="no_docs")],
 )
-def test_maintaining_features(
+def test_pyproject_features(
     render_template: Callable[..., Path],
     snapshot: SnapshotAssertion,
     *,
